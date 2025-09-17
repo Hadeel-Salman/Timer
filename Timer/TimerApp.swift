9cb00531b80f9544a109dbdcd6425c59f3f793bf
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-17 20:47:14 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Hadeel on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
