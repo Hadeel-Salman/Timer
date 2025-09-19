840116844492140b4b0d93df2a933083c98aef17
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-19 22:57:02 +0300

    Added a TimerItemView

---
+++ Timer/view/TimerItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerItemView.swift
+//  Timer
+//
+//  Created by Hadeel on 19/09/2025.
+//
+
+import SwiftUI
+
+struct TimerItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerItemView()
+}
