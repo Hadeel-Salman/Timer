bcf8761b9dcb3cdc670e4f156e7927d0169c14f8
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-19 15:30:06 +0300

    newTimerView added

---
+++ Timer/components/TextFieldStyle.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldStyle.swift
+//  Timer
+//
+//  Created by Hadeel on 19/09/2025.
+//
+
+import SwiftUI
+
+struct TextFieldStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFieldStyle()
+}
